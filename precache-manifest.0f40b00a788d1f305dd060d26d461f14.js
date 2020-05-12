88f8d418ad3414422ff56aecabbd713edbd52ba2
divcraft <t.ogonowski@wp.pl>
2020-05-12 20:00:33 +0200

    Updates

---
+++ precache-manifest.0f40b00a788d1f305dd060d26d461f14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da061405ad42ca11e5430bdef07d9de",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "7df1a140edb115d40029",
+    "url": "/calculator/static/css/main.14aaaa36.chunk.css"
+  },
+  {
+    "revision": "cabc178def831b2c711a",
+    "url": "/calculator/static/js/2.3c1e135f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.3c1e135f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df1a140edb115d40029",
+    "url": "/calculator/static/js/main.49d2b2de.chunk.js"
+  },
+  {
+    "revision": "d60859d8a9f5f0e7455b",
+    "url": "/calculator/static/js/runtime-main.1fde7ec2.js"
+  },
+  {
+    "revision": "061943c7e1da2f4da9382aaa8a0e946c",
+    "url": "/calculator/static/media/logo.061943c7.svg"
+  }
+]);
